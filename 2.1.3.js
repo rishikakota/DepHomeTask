8f00aad9402aac5b4ab070111e5bc2192d00323e
rishikakota <81610578+rishikakota@users.noreply.github.com>
2021-04-29 13:16:56 +0530

    Add files via upload

---
+++ 2.1.3.js
@@ -0,0 +1,25 @@
+function basicOp(operation, value1, value2)
+{
+  // Code
+  return eval(value1+operation+value2);
+}
+
+
+function basicOp(operation, value1, value2)
+{
+  // Code
+  if(operation == "+"){
+    return value1+value2;
+  }
+  else if(operation == "-"){
+    return value1-value2;
+  }
+  else if(operation == "*"){
+    return value1*value2;
+  }
+  else if(operation == "/"){
+    return value1/value2;
+  }
+  
+  
+}
---
+++ 2.1.4.js
@@ -0,0 +1,12 @@
+function rentalCarCost(d) {
+  // Your solution here
+  if(d<3){
+    return (d*40);
+  }
+  else if(d>=3 && d<7){
+    return (d*40)-20;
+  }
+  else if(d>=7){
+    return (d*40)-50;
+  }
+}
