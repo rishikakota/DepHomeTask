3856cf3ececff8002f136511351554b613ea3833
rishikakota <81610578+rishikakota@users.noreply.github.com>
2021-06-22 14:18:23 +0530

    Add files via upload

---
+++ js-assignments/task/01-strings-tasks.js
@@ -0,0 +1,408 @@
+"use strict";
+
+/********************************************************************************************
+ *                                                                                          *
+ * Plese read the following tutorial before implementing tasks:                             *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String  *
+ *                                                                                          *
+ ********************************************************************************************/
+
+/**
+ * Returns the result of concatenation of two strings.
+ *
+ * @param {string} value1
+ * @param {string} value2
+ * @return {string}
+ *
+ * @example
+ *   'aa', 'bb' => 'aabb'
+ *   'aa',''    => 'aa'
+ *   '',  'bb'  => 'bb'
+ */
+function concatenateStrings(value1, value2) {
+  //throw new Error('Not implemented');
+  return value1 + value2;
+}
+
+/**
+ * Returns the length of given string.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example
+ *   'aaaaa' => 5
+ *   'b'     => 1
+ *   ''      => 0
+ */
+function getStringLength(value) {
+  //throw new Error("Not implemented");
+  return value.length;
+}
+
+/**
+ * Returns the result of string template and given parameters firstName and lastName.
+ * Please do not use concatenation, use template string :
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/template_strings
+ *
+ * @param {string} firstName
+ * @param {string} lastName
+ * @return {string}
+ *
+ * @example
+ *   'John','Doe'      => 'Hello, John Doe!'
+ *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
+ */
+function getStringFromTemplate(firstName, lastName) {
+  //throw new Error("Not implemented");
+  return `Hello, ${firstName} ${lastName}!`;
+}
+
+/**
+ * Extracts a name from template string 'Hello, First_Name Last_Name!'.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'Hello, John Doe!' => 'John Doe'
+ *   'Hello, Chuck Norris!' => 'Chuck Norris'
+ */
+function extractNameFromTemplate(value) {
+  //throw new Error("Not implemented");
+  //let { firstName } = value;
+  //let { lastName } = value;
+  var result = "";
+  let i = 7;
+  while (i < value.length - 1) {
+    result += value[i];
+    i = i + 1;
+  }
+  return result;
+}
+
+/**
+ * Returns a first char of the given string.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'John Doe'  => 'J'
+ *   'cat'       => 'c'
+ */
+function getFirstChar(value) {
+  //throw new Error("Not implemented");
+  return value[0];
+}
+
+/**
+ * Removes a leading and trailing whitespace characters from string.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   '  Abracadabra'    => 'Abracadabra'
+ *   'cat'              => 'cat'
+ *   '\tHello, World! ' => 'Hello, World!'
+ */
+function removeLeadingAndTrailingWhitespaces(value) {
+  //throw new Error("Not implemented");
+  return value.trim();
+}
+
+/**
+ * Returns a string that repeated the specified number of times.
+ *
+ * @param {string} value
+ * @param {string} count
+ * @return {string}
+ *
+ * @example
+ *   'A', 5  => 'AAAAA'
+ *   'cat', 3 => 'catcatcat'
+ */
+function repeatString(value, count) {
+  //throw new Error("Not implemented");
+  var result = "";
+
+  while (count > 0) {
+    result += value;
+    count -= 1;
+  }
+  return result;
+}
+
+/**
+ * Remove the first occurrence of string inside another string
+ *
+ * @param {string} str
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'To be or not to be', 'not'  => 'To be or to be'
+ *   'I like legends', 'end' => 'I like legs',
+ *   'ABABAB','BA' => 'ABAB'
+ */
+function removeFirstOccurrences(str, value) {
+  //throw new Error("Not implemented");
+  let i = 0;
+  const forward = value.length;
+  var result = "";
+  while (i < str.length - forward) {
+    //console.log(str.slice(i, i + forward));
+    if (str.slice(i, i + forward) != value) {
+      result = result + str[i];
+    } else {
+      i = i + forward;
+      break;
+    }
+    i += 1;
+  }
+
+  if (i < str.length) {
+    result += str.slice(i, str.length);
+  }
+
+  return result;
+}
+
+/**
+ * Remove the first and last angle brackets from tag string
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *   '<div>' => 'div'
+ *   '<span>' => 'span'
+ *   '<a>' => 'a'
+ */
+function unbracketTag(str) {
+  //throw new Error("Not implemented");
+  return str.slice(1, str.length - 1);
+}
+
+/**
+ * Converts all characters of the specified string into the upper case
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *   'Thunderstruck' => 'THUNDERSTRUCK'
+ *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ */
+function convertToUpperCase(str) {
+  //throw new Error("Not implemented");
+  return str.toUpperCase();
+}
+
+/**
+ * Extracts e-mails from single string with e-mails list delimeted by semicolons
+ *
+ * @param {string} str
+ * @return {array}
+ *
+ * @example
+ *   'angus.young@gmail.com;brian.johnson@hotmail.com;bon.scott@yahoo.com' => ['angus.young@gmail.com', 'brian.johnson@hotmail.com', 'bon.scott@yahoo.com']
+ *   'info@gmail.com' => ['info@gmail.com']
+ */
+function extractEmails(str) {
+  //throw new Error("Not implemented");
+  return str.split(";");
+}
+
+/**
+ * Returns the string representation of rectangle with specified width and height
+ * using pseudograhic chars
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {string}
+ *
+ * @example
+ *
+ *            '┌────┐\n'+
+ *  (6,4) =>  '│    │\n'+
+ *            '│    │\n'+
+ *            '└────┘\n'
+ *
+ *  (2,2) =>  '┌┐\n'+
+ *            '└┘\n'
+ *
+ *             '┌──────────┐\n'+
+ *  (12,3) =>  '│          │\n'+
+ *             '└──────────┘\n'
+ *
+ */
+function getRectangleString(width, height) {
+  //throw new Error("Not implemented");
+
+  let horisChar = "─",
+    verticChar = "│",
+    breakChar = "\n",
+    leftangleup = "┌",
+    leftanglelow = "└",
+    rigthangleup = "┐",
+    rigthanglelow = "┘",
+    whitespace = " ";
+  let str = "";
+  let str1 =
+    leftangleup + horisChar.repeat(width - 2) + rigthangleup + breakChar;
+  let str2 = "";
+  for (let i = 0; i < height - 2; i++) {
+    str2 += verticChar + whitespace.repeat(width - 2) + verticChar + breakChar;
+  }
+  let str3 =
+    leftanglelow + horisChar.repeat(width - 2) + rigthanglelow + breakChar;
+  str = str1 + str2 + str3;
+  return str;
+}
+
+/**
+ * Encode specified string with ROT13 cipher
+ * See details:  https://en.wikipedia.org/wiki/ROT13
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *
+ *   'hello' => 'uryyb'
+ *   'Why did the chicken cross the road?' => 'Jul qvq gur puvpxra pebff gur ebnq?'
+ *   'Gb trg gb gur bgure fvqr!' => 'To get to the other side!'
+ *   'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+ *
+ */
+function encodeToRot13(str) {
+  //throw new Error("Not implemented");
+  let str1 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+  let str2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  return str.replace(/[A-Z]/gi, (c) => str1[str2.indexOf(c)]);
+}
+
+/**
+ * Returns true if the value is string; otherwise false.
+ * @param {string} value
+ * @return {boolean}
+ *
+ * @example
+ *   isString() => false
+ *   isString(null) => false
+ *   isString([]) => false
+ *   isString({}) => false
+ *   isString('test') => true
+ *   isString(new String('test')) => true
+ */
+function isString(value) {
+  //throw new Error("Not implemented");
+  let type_of_value = typeof value;
+  if (type_of_value == "string") return true;
+  else if (Object.prototype.toString.call(value) === "[object String]")
+    return true;
+  else return false;
+}
+
+/**
+ * Returns playid card id.
+ *
+ * Playing cards inittial deck inclides the cards in the following order:
+ *
+ *  'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
+ *  'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
+ *  'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
+ *  'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'
+ *
+ * (see https://en.wikipedia.org/wiki/Standard_52-card_deck)
+ * Function returns the zero-based index of specified card in the initial deck above.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example
+ *   'A♣' => 0
+ *   '2♣' => 1
+ *   '3♣' => 2
+ *     ...
+ *   'Q♠' => 50
+ *   'K♠' => 51
+ */
+function getCardId(value) {
+  //throw new Error("Not implemented");
+  let arr1 = [
+    "A♣",
+    "2♣",
+    "3♣",
+    "4♣",
+    "5♣",
+    "6♣",
+    "7♣",
+    "8♣",
+    "9♣",
+    "10♣",
+    "J♣",
+    "Q♣",
+    "K♣",
+    "A♦",
+    "2♦",
+    "3♦",
+    "4♦",
+    "5♦",
+    "6♦",
+    "7♦",
+    "8♦",
+    "9♦",
+    "10♦",
+    "J♦",
+    "Q♦",
+    "K♦",
+    "A♥",
+    "2♥",
+    "3♥",
+    "4♥",
+    "5♥",
+    "6♥",
+    "7♥",
+    "8♥",
+    "9♥",
+    "10♥",
+    "J♥",
+    "Q♥",
+    "K♥",
+    "A♠",
+    "2♠",
+    "3♠",
+    "4♠",
+    "5♠",
+    "6♠",
+    "7♠",
+    "8♠",
+    "9♠",
+    "10♠",
+    "J♠",
+    "Q♠",
+    "K♠",
+  ];
+  return arr1.indexOf(value);
+}
+
+module.exports = {
+  concatenateStrings: concatenateStrings,
+  getStringLength: getStringLength,
+  getStringFromTemplate: getStringFromTemplate,
+  extractNameFromTemplate: extractNameFromTemplate,
+  getFirstChar: getFirstChar,
+  removeLeadingAndTrailingWhitespaces: removeLeadingAndTrailingWhitespaces,
+  repeatString: repeatString,
+  removeFirstOccurrences: removeFirstOccurrences,
+  unbracketTag: unbracketTag,
+  convertToUpperCase: convertToUpperCase,
+  extractEmails: extractEmails,
+  getRectangleString: getRectangleString,
+  encodeToRot13: encodeToRot13,
+  isString: isString,
+  getCardId: getCardId,
+};
---
+++ js-assignments/task/03-date-tasks.js
@@ -0,0 +1,140 @@
+"use strict";
+
+/********************************************************************************************
+ *                                                                                          *
+ * Plese read the following tutorial before implementing tasks:                             *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Numbers_and_dates#Date_object
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date    *
+ *                                                                                          *
+ ********************************************************************************************/
+
+/**
+ * Parses a rfc2822 string date representation into date value
+ * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
+ *
+ * @param {string} value
+ * @return {date}
+ *
+ * @example:
+ *    'December 17, 1995 03:24:00'    => Date()
+ *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
+ *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
+ */
+function parseDataFromRfc2822(value) {
+  return new Date(value);
+  throw new Error("Not implemented");
+}
+
+/**
+ * Parses an ISO 8601 string date representation into date value
+ * For ISO 8601 date specification refer to : https://en.wikipedia.org/wiki/ISO_8601
+ *
+ * @param {string} value
+ * @return {date}
+ *
+ * @example :
+ *    '2016-01-19T16:07:37+00:00'    => Date()
+ *    '2016-01-19T08:07:37Z' => Date()
+ */
+function parseDataFromIso8601(value) {
+  return new Date(value);
+  throw new Error("Not implemented");
+}
+
+/**
+ * Returns true if specified date is leap year and false otherwise
+ * Please find algorithm here: https://en.wikipedia.org/wiki/Leap_year#Algorithm
+ *
+ * @param {date} date
+ * @return {bool}
+ *
+ * @example :
+ *    Date(1900,1,1)    => false
+ *    Date(2000,1,1)    => true
+ *    Date(2001,1,1)    => false
+ *    Date(2012,1,1)    => true
+ *    Date(2015,1,1)    => false
+ */
+function isLeapYear(date) {
+  let currentDate = new Date(date);
+  let year = currentDate.getFullYear();
+  if (year % 4 != 0) return false;
+  else if (year % 100 != 0) return true;
+  else if (year % 400 != 0) return false;
+  else return true;
+  throw new Error("Not implemented");
+}
+
+/**
+ * Returns the string represention of the timespan between two dates.
+ * The format of output string is "HH:mm:ss.sss"
+ *
+ * @param {date} startDate
+ * @param {date} endDate
+ * @return {string}
+ *
+ * @example:
+ *    Date(2000,1,1,10,0,0),  Date(2000,1,1,11,0,0)   => "01:00:00.000"
+ *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,30,0)       => "00:30:00.000"
+ *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,20)        => "00:00:20.000"
+ *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
+ *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
+ */
+function timeSpanToString(startDate, endDate) {
+  let startDateNew = new Date(startDate);
+  let endDateNew = new Date(endDate);
+  let milisecondLeft = endDateNew.getTime() - startDateNew.getTime();
+  let milliseconds = parseInt(milisecondLeft % 1000),
+    seconds = parseInt((milisecondLeft / 1000) % 60),
+    minutes = parseInt((milisecondLeft / (1000 * 60)) % 60),
+    hours = parseInt((milisecondLeft / (1000 * 60 * 60)) % 24);
+  if (startDateNew.getDay() !== endDateNew.getDay()) hours = hours + 24;
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  milliseconds =
+    milliseconds < 10
+      ? "00" + milliseconds
+      : milliseconds < 100
+      ? "0" + milliseconds
+      : milliseconds;
+  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+  throw new Error("Not implemented");
+}
+
+/**
+ * Returns the angle (in radians) between the hands of an analog clock for the specified Greenwich time.
+ * If you have problem with solution please read: https://en.wikipedia.org/wiki/Clock_angle_problem
+ *
+ * @param {date} date
+ * @return {number}
+ *
+ * @example:
+ *    Date.UTC(2016,2,5, 0, 0) => 0
+ *    Date.UTC(2016,3,5, 3, 0) => Math.PI/2
+ *    Date.UTC(2016,3,5,18, 0) => Math.PI
+ *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
+ */
+function angleBetweenClockHands(date) {
+  let newDate = new Date(date);
+  let onehourangle = 30;
+  let oneminuteangle = 6;
+  let endHour = newDate.getUTCHours();
+  endHour > 12 ? (endHour = endHour - 12) : endHour;
+  let hours = endHour + newDate.getUTCMinutes() / 60;
+  let hourAngle = hours * onehourangle;
+  let minuteAngle = newDate.getUTCMinutes() * oneminuteangle;
+  let angle = Math.abs(hourAngle - minuteAngle);
+  let radians = (angle * Math.PI) / 180;
+  let result = radians > Math.PI ? radians - Math.PI : radians;
+  return result;
+  throw new Error("Not implemented");
+}
+
+module.exports = {
+  parseDataFromRfc2822: parseDataFromRfc2822,
+  parseDataFromIso8601: parseDataFromIso8601,
+  isLeapYear: isLeapYear,
+  timeSpanToString: timeSpanToString,
+  angleBetweenClockHands: angleBetweenClockHands,
+};
